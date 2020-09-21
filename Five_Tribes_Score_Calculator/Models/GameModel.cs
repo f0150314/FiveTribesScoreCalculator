703d953772d0230682263b0e84c9853d01d609b0
John Chen <f0150314@gmail.com>
2020-09-21 14:40:57 +1000

    add new models and change launch screen and app icon.

--- Five_Tribes_Score_Calculator/Models/GameModel.cs
+++ Five_Tribes_Score_Calculator/Models/GameModel.cs
@@ -3,6 +3,15 @@ namespace Five_Tribes_Score_Calculator.Models
 {
     public class GameModel
     {
+        public enum GameType
+        {
+            FT,
+            AQ,
+            WS
+        }
+
+        public int PlayerCount { get; set; }
+
         public GameModel()
         {
         }
---
+++ Five_Tribes_Score_Calculator/Models/ScoreModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Five_Tribes_Score_Calculator.Models
+{
+    public class ScoreModel
+    {
+        public int Coins { get; set; }
+        public int Viziers { get; set; }
+        public int Artisans { get; set; }
+        public int Elders { get; set; }
+        public int Djinns { get; set; }
+        public int PalmTrees { get; set; }
+        public int Palaces { get; set; }
+        public int Camels { get; set; }
+        public int PreciousItems { get; set; }
+        public int Resources { get; set; }
+
+        public ScoreModel()
+        {
+        }
+
+        //Calculate score for Viziers
+        private void CalculateViziers()
+        {
+
+        }
+
+        //Calculate score for Elders
+        private void CalculateElders()
+        {
+
+        }
+
+        //Calculate score for Artisans
+        private void CalculateArtisans()
+        {
+
+        }
+    }
+}
